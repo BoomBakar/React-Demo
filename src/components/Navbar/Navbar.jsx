d4015c98a0ede237d6f1ed7962e3e3c755ac05d5
BoomBakar <abubakardar608@gmail.com>
2023-08-22 14:11:34 +0500

    Main files cleaned up

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      {/* <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          <img src={logo} alt="logo" className="logo" />
+          <span className="logo-text">React</span>
+        </Link>
+      </div> */}
+      <h2>Navbar</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
